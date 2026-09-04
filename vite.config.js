e01e317ac82a6c1f5bbb6112c45c28f207c646a2
mariainsana <tu-correo...>
2026-09-04 00:10:08 -0700

    Icono de la APP

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,38 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Farmeo de Dittos',
+        short_name: 'Farmeo Ditto',
+        description:
+          'Guía insana hecha por @andreamariauwu para el farmeo de Dittos en Hoenn. La guía sigue en producción, así que esperen futuras actualizaciones y mejoras.',
+        theme_color: '#05111d',
+        background_color: '#05111d',
+        display: 'standalone',
+        start_url: '/',
+        scope: '/',
+        icons: [
+          {
+            src: '/icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+          {
+            src: '/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
 })
